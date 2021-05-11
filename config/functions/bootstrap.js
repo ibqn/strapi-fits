641ead7deddb4b64e75d774cf28dd5363884d268
ibqn <evgen.ibqn@gmail.com>
2021-05-12 00:46:11 +0200

    populating test data

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,6 @@
-'use strict';
+'use strict'
+
+const data = require('../../data/products')
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +12,27 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = async () => {
+  const { products } = data
+  console.log(`🌱 Inserting Seed Data: ${products.length} Products`)
+
+  products.forEach(async (product) => {
+    const { name, description, price, status } = product
+
+    const foundProduct = await strapi.services.product.findOne({ name }, [
+      'name',
+    ])
+
+    if (foundProduct !== null) {
+      console.log(`${name} already exists`)
+      return
+    }
+
+    strapi.services.product.create({
+      name,
+      description,
+      status,
+      price,
+    })
+  })
+}
---
+++ data/products.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    name: 'Yeti Hondo',
+    description: 'soo nice',
+    status: 'Available',
+    price: 3423,
+    photo: {
+      filename: 'hondo.jpg',
+      url: 'http://res.cloudinary.com/wesbos/image/upload/v1576791335/sick-fits-keystone/5dfbed262849d7961377c2c0.jpg',
+    },
+  },
+]
+
+exports.products = products
