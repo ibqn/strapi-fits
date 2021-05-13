77da9192f0a03c8eefabd4f6f81a9e4715092f3c
ibqn <evgen.ibqn@gmail.com>
2021-05-14 00:33:09 +0200

    update seed data. include images

--- data/products.js
+++ data/products.js
@@ -4,10 +4,7 @@ const products = [
     description: 'soo nice',
     status: 'Available',
     price: 3423,
-    photo: {
-      filename: 'hondo.jpg',
-      url: 'http://res.cloudinary.com/wesbos/image/upload/v1576791335/sick-fits-keystone/5dfbed262849d7961377c2c0.jpg',
-    },
+    image: 'hondo.jpg',
   },
 ]
 
