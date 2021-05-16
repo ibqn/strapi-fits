3a9bbbed7e393bb69e861cafc1e42aa02fdec4c0
ibqn <evgen.ibqn@gmail.com>
2021-05-16 12:16:52 +0200

    populate strapi with seed data

--- cli/seed.js
+++ cli/seed.js
@@ -1,5 +1,9 @@
 const strapiInstance = require('strapi')
 
+const path = require('path')
+const fs = require('fs')
+const mime = require('mime-types')
+
 const data = require('../data/products')
 
 strapiInstance()
@@ -8,8 +12,10 @@ strapiInstance()
     const { products } = data
     console.log(`🌱 Inserting Seed Data: ${products.length} Products`)
 
-    for (const product of products) {
-      const { name, description, price, status } = product
+    let skippedProducts = 0
+
+    for (const productData of products) {
+      const { name, description, price, status, image } = productData
 
       const foundProduct = await strapi.services.product.findOne({ name }, [
         'name',
@@ -17,18 +23,48 @@ strapiInstance()
 
       if (foundProduct !== null) {
         console.log(`  🛍️  Product already exists: ${name}`)
+        skippedProducts++
         continue
       }
 
-      strapi.services.product.create({
+      const imgPath = path.join('data', image)
+      const imgBuffer = await fs.statSync(imgPath)
+      const imgName = path.basename(imgPath)
+
+      const imageData = {
+        path: imgPath,
+        name: imgName,
+        type: mime.lookup(imgPath),
+        size: imgBuffer.size,
+      }
+
+      // console.log('image', imageData)
+
+      const productImage = await strapi.services['product-image'].create(
+        {
+          altText: name,
+        },
+        {
+          files: { image: imageData },
+        }
+      )
+      console.log('product image', productImage)
+
+      console.log(`  🛍️  Adding Product: ${name}`)
+
+      const product = await strapi.services.product.create({
         name,
         description,
         status,
         price,
+        images: [productImage],
       })
-
-      console.log(`  🛍️  Adding Product: ${name}`)
+      console.log('product', product)
     }
-    // exit with 0 code
+
+    console.log(
+      `✅  Seed Data Inserted: ${products.length - skippedProducts} Products`
+    )
+
     strapi.stop(0)
   })
